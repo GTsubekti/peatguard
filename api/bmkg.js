2201405357f416d568167047f2c0c098459f1189
GTsubekti <136559007+GTsubekti@users.noreply.github.com>
2026-04-24 13:51:32 +0700

    fix kode adm4 BMKG

--- api/bmkg.js
+++ api/bmkg.js
@@ -1,16 +1,16 @@
 const REGIONS_BMKG = [
-  { id:1,  name:"Riau",        kode:"1402" },
-  { id:2,  name:"Kalteng",     kode:"6271" },
-  { id:3,  name:"Kalbar",      kode:"6101" },
-  { id:4,  name:"Papua Barat", kode:"9201" },
-  { id:5,  name:"Jambi",       kode:"1501" },
-  { id:6,  name:"Sumsel",      kode:"1601" },
-  { id:7,  name:"Kalsel",      kode:"6301" },
-  { id:8,  name:"Kaltim",      kode:"6401" },
-  { id:9,  name:"Aceh",        kode:"1101" },
-  { id:10, name:"Papua Tgh",   kode:"9401" },
-  { id:11, name:"Sumut",       kode:"1201" },
-  { id:12, name:"Bengkulu",    kode:"1701" },
+  { id:1,  name:"Riau",        kode:"14.71.04.1001" },
+  { id:2,  name:"Kalteng",     kode:"62.71.01.1001" },
+  { id:3,  name:"Kalbar",      kode:"61.71.01.1001" },
+  { id:4,  name:"Papua Barat", kode:"92.01.01.1001" },
+  { id:5,  name:"Jambi",       kode:"15.71.01.1001" },
+  { id:6,  name:"Sumsel",      kode:"16.71.01.1001" },
+  { id:7,  name:"Kalsel",      kode:"63.71.01.1001" },
+  { id:8,  name:"Kaltim",      kode:"64.71.01.1001" },
+  { id:9,  name:"Aceh",        kode:"11.71.01.1001" },
+  { id:10, name:"Papua Tgh",   kode:"94.01.01.1001" },
+  { id:11, name:"Sumut",       kode:"12.71.01.1001" },
+  { id:12, name:"Bengkulu",    kode:"17.71.01.1001" },
 ];
 
 export default async function handler(req, res) {
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     const results = await Promise.all(
       REGIONS_BMKG.map(async r => {
         try {
-          const url = `https://api.bmkg.go.id/publik/prakiraan-cuaca?adm2=${r.kode}`;
+          const url = `https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4=${r.kode}`;
           const response = await fetch(url);
           const data = await response.json();
           const item = data.data?.[0]?.cuaca?.[0]?.[0];
